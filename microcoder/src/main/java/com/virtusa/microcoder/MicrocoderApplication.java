ea2fbde76102e72ac3910dccd3dbde40b40979ce
Suraj Mourya <59648651+imsurajmourya@users.noreply.github.com>
2023-08-03 15:50:53 +0530

    Add files via upload

---
+++ microcoder/src/main/java/com/virtusa/microcoder/MicrocoderApplication.java
@@ -0,0 +1,13 @@
+package com.virtusa.microcoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrocoderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrocoderApplication.class, args);
+	}
+
+}
---
+++ microcoder/src/test/java/com/virtusa/microcoder/MicrocoderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virtusa.microcoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrocoderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
